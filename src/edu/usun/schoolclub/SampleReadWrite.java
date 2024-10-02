435630f77e5fe0437f65632d767dd5b3a5751006
igors <igors@10.0.0.39>
2024-10-02 17:48:43 -0400

    adjust file path for samples

--- src/edu/usun/schoolclub/SampleReadWrite.java
+++ src/edu/usun/schoolclub/SampleReadWrite.java
@@ -7,12 +7,13 @@ public class SampleReadWrite {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new FileInputStream(
-				"C:\\repo\\github\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"
+				"C:\\Users\\igors\\git\\usunschoolclub\\src\\edu\\usun\\schoolclub\\SampleReadWrite.txt"
 		));
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 			System.out.println("Read line: " + line);
 		}
+		sc.close();
 	}
 
 }
